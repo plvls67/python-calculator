a114fd7f71e1d3af833d0650d23fa018f0ce03e9
plvls67 <plvls67@gmail.com>
2026-04-24 13:30:57 +0300

    Initial commit: Python calculator

---
+++ απλή_αριθμομηχανή.py
@@ -0,0 +1,32 @@
+# απλή_αριθμομηχανή.py
+
+print("Απλή Αριθμομηχανή")
+print("Επιλογές: +, -, *, /")
+
+# 1. Παίρνουμε είσοδο από τον χρήστη
+num1 = float(input("Δώσε τον πρώτο αριθμό: "))
+op = input("Δώσε πράξη (+, -, *, /): ")
+num2 = float(input("Δώσε τον δεύτερο αριθμό: "))
+
+# 2. Υπολογισμός ανάλογα με την πράξη
+if op == "+":
+    result = num1 + num2
+    print("Αποτέλεσμα:", result)
+
+elif op == "-":
+    result = num1 - num2
+    print("Αποτέλεσμα:", result)
+
+elif op == "*":
+    result = num1 * num2
+    print("Αποτέλεσμα:", result)
+
+elif op == "/":
+    if num2 == 0:
+        print("Σφάλμα: δεν γίνεται διαίρεση με το 0.")
+    else:
+        result = num1 / num2
+        print("Αποτέλεσμα:", result)
+
+else:
+    print("Σφάλμα: άγνωστη πράξη.")
